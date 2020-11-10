40376276cb3028a585ab0661ef933c855f92a5cf
Lorenzo <47974973+Freemantles@users.noreply.github.com>
2020-11-10 17:23:40 +0100

    1.12

--- navscroll.js
+++ navscroll.js
@@ -1,3 +1,12 @@
-window.onscroll = function(e) {
-      document.getElementById("fullna").classList.add("backcol");
-   } 
+function toggleMenu() {
+  var menuBox = document.getElementById('mobile-nav-popup');
+  var menuIcon = document.getElementById('hamb-menu');
+  if(menuBox.style.display == "none") {
+    menuBox.style.display = "block";
+    menuIcon.src = "close.svg"
+  }
+  else {
+    menuBox.style.display = "none";
+    menuIcon.src = "mobile-menu.svg"
+  }
+}
