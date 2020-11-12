d77a42e408da7f50c8afc6261cfe37c155aa1abf
Lorenzo <47974973+Freemantles@users.noreply.github.com>
2020-11-12 11:37:05 +0100

    1.4

--- navscroll.js
+++ navscroll.js
@@ -4,12 +4,18 @@ function toggleMenu() {
   var menuIconClose = document.getElementById('hamb-menu-close');
   if(menuBox.style.display == "none") {
     menuBox.style.display = "block";
-    menuIconOpen.style.opacity = "0"
-    menuIconClose.style.opacity = "1"
+    menuIconOpen.style.opacity = "0";
+    menuIconClose.style.opacity = "1";
   }
   else {
     menuBox.style.display = "none";
-    menuIconOpen.style.opacity = "1"
-    menuIconClose.style.opacity = "0"
+    menuIconOpen.style.opacity = "1";
+    menuIconClose.style.opacity = "0";
   }
+}
+
+
+function closeMenu() {
+	var menuBox = document.getElementById('mobile-nav-popup');
+		menuBox.style.display = "none";
 }
